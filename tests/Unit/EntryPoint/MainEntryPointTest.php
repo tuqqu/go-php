dce9544ee6b6f326c6e0acbd5122ecbc4ac96e59
tuqqu <artkurbidaev@gmail.com>
2022-04-02 17:40:30 +0300

    Unit tests, phpunit config

---
+++ tests/Unit/EntryPoint/MainEntryPointTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GoPhp\Tests\Unit\EntryPoint;
+
+use GoPhp\EntryPoint\MainEntryPoint;
+use GoPhp\GoType\BasicType;
+use GoPhp\GoValue\Func\Param;
+use GoPhp\GoValue\Func\Params;
+use GoPhp\GoValue\Func\Signature;
+use PHPUnit\Framework\TestCase;
+
+final class MainEntryPointTest extends TestCase
+{
+    public function testSuccessValidate(): void
+    {
+        $validator = new MainEntryPoint();
+        $signature = new Signature(
+            new Params([]),
+            new Params([]),
+        );
+
+        self::assertTrue($validator->validate('main', 'main', $signature));
+    }
+
+    public function testWrongSignature(): void
+    {
+        $validator = new MainEntryPoint();
+        $signature = new Signature(
+            new Params([new Param(BasicType::Int)]),
+            new Params([new Param(BasicType::Int)]),
+        );
+
+        self::assertFalse($validator->validate('main', 'main', $signature));
+    }
+
+    public function testWrongPackageName(): void
+    {
+        $validator = new MainEntryPoint();
+        $signature = new Signature(
+            new Params([]),
+            new Params([]),
+        );
+
+        self::assertFalse($validator->validate('fmt', 'main', $signature));
+    }
+}
---
+++ tests/Unit/Env/ValueTableTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GoPhp\Tests\Unit\Env;
+
+use GoPhp\Env\EnvValue\EnvValue;
+use GoPhp\Env\EnvValue\ImmutableValue;
+use GoPhp\Env\Error\AlreadyDefinedError;
+use GoPhp\Env\Error\UndefinedValueError;
+use GoPhp\Env\ValueTable;
+use GoPhp\GoType\BasicType;
+use PHPUnit\Framework\TestCase;
+
+final class ValueTableTest extends TestCase
+{
+    private ValueTable $table;
+    private EnvValue $valueA;
+    private EnvValue $valueB;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->table = new ValueTable();
+        $this->valueA = $this->createEnvValue('a');
+        $this->valueB = $this->createEnvValue('b');
+
+        $this->table->add($this->valueA);
+        $this->table->add($this->valueB);
+    }
+
+    public function testHas(): void
+    {
+        self::assertTrue($this->table->has('a'));
+        self::assertTrue($this->table->has('b'));
+
+        self::assertFalse($this->table->has('c'));
+    }
+
+    public function testGet(): void
+    {
+        self::assertEquals($this->valueA, $this->table->tryGet('a'));
+        self::assertEquals($this->valueB, $this->table->get('b'));
+
+        self::assertNull($this->table->tryGet('c'));
+
+        $this->expectException(UndefinedValueError::class);
+        $this->table->get('c');
+    }
+
+    public function testAdd(): void
+    {
+        $this->expectException(AlreadyDefinedError::class);
+        $this->table->add($this->createEnvValue('a'));
+    }
+
+    private function createEnvValue(string $name): EnvValue
+    {
+        return new ImmutableValue(
+            $name,
+            BasicType::Int,
+            BasicType::Int->defaultValue(),
+        );
+    }
+}
